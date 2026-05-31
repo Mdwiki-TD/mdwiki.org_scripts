8370a468024075f4a691f37f525e94b7b9412136
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-01 01:40:37 +0300

    .

--- tests/unit/shared/new_updater/tests/test_work_on_text.py
+++ tests/unit/shared/new_updater/tests/test_work_on_text.py
@@ -39,3 +39,21 @@ def test_2():
         f.write(newtext)
 
     assert newtext.strip() == expected.strip()
+
+
+def test_3():
+
+    Dir = Path(__file__).parent
+
+    text_file = Dir / "texts/test3/work_on_text_source.wiki"
+    expected_file = Dir / "texts/test3/work_on_text_expected.wiki"
+
+    text = text_file.read_text(encoding="utf-8")
+    expected = expected_file.read_text(encoding="utf-8")
+
+    newtext = work_on_text("test", text)
+
+    with open(Dir / "texts/test3/work_on_text_result.wiki", "w", encoding="utf-8") as f:
+        f.write(newtext)
+
+    assert newtext.strip() == expected.strip()
