caf54f900d3f22a3805ec09692e61d46c698fc89
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-02 02:09:27 +0300

    .

--- flask_app/main_app/db/models/jobs.py
+++ flask_app/main_app/db/models/jobs.py
@@ -3,11 +3,9 @@ from __future__ import annotations
 import logging
 
 from sqlalchemy import Column, DateTime, Integer, String, func
-
 from ...extensions import db
 
 logger = logging.getLogger(__name__)
-
 
 class JobRecord(db.Model):
     """
