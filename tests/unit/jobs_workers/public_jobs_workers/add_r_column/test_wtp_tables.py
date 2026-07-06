05feebfcda626eacf937096cd73158e8a8ee9f64
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-06 07:21:47 +0300

    Update test_wtp_tables.py

--- tests/unit/jobs_workers/public_jobs_workers/add_r_column/test_wtp_tables.py
+++ tests/unit/jobs_workers/public_jobs_workers/add_r_column/test_wtp_tables.py
@@ -9,13 +9,16 @@ from wikitextparser._cell import Cell
 table = wtp.Table("""
 {|
 | A || colspan="2" | B
-|-
-| C || D || E
+|- test="test"
+| class='ss' | C || D || E
 |}
 """)
 
 def test_table_data():
     assert table.data() == [['A', 'B', 'B'], ['C', 'D', 'E']]
+    assert table.row_attrs == [{"test":"test"}]
+    table.row_attrs = [{"test":"test!"}]
+    assert '|- test="test!"' in table.string
 
 def test_table_cells_len():
     cells = table.cells()
@@ -27,3 +30,25 @@ def test_table_cells_len():
 def test_table_cells():
     cells = table.cells()
     assert cells[0][2].string == Cell('|| colspan="2" | B').string
+
+
+table_header = wtp.Table("""
+{|
+! Head1
+! Head2
+|-
+| C || D
+|}
+""")
+
+def test_table_head_cells():
+    cells = table_header.cells()
+    assert cells[0][1].string == Cell('\n! Head2').string
+
+def test_table_head_cells_text():
+    cells = table_header.cells()
+
+    assert cells[0][1].is_header is True
+
+    assert cells[1][1].string == Cell('|| D').string
+    assert Cell('|| D').is_header is False
