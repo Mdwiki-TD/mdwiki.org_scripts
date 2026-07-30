eb98dce07efc1af9495ff37169679a6324c3c713
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-30 03:06:18 +0300

    Update worker.py

--- src/main_app/jobs_workers/public_jobs_workers/fixred_all/worker.py
+++ src/main_app/jobs_workers/public_jobs_workers/fixred_all/worker.py
@@ -52,7 +52,7 @@ class FixRedAllWorker(BaseObjectsJobWorker):
 
         state = RunState()
         titles = list(
-            self.site.allpages(
+            self.site.allpages( # type: ignore
                 start="!",
                 namespace=0,
                 filterredir="nonredirects",
