70f3382ad886c6c97bcdb59eba97a5b550d60276
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-14 00:24:14 +0300

    .

--- src/main_app/services/auth/utils.py
+++ src/main_app/services/auth/utils.py
@@ -12,8 +12,8 @@ from flask import g, request, session
 
 from ...config import settings
 from ..core.cookies import extract_user_id
-from .auth_users_service import AuthUserService
 from .current_user import CurrentUser
+from .token_manager import TokenManager
 
 FuncType = TypeVar("FuncType", bound=Callable[..., Any])
 
@@ -63,11 +63,13 @@ def _get_user_id() -> None | int:
 
 
 def _build_current_user(user_id) -> None | CurrentUser:
-    """Fetch user from Service Layer and hydrate session/g context."""
+    """
+    Fetch user from Service Layer and hydrate session/g context.
+    """
     if user_id is None:
         return None
 
-    user = AuthUserService().get_authenticated_user(user_id)
+    user = TokenManager().get_authenticated_user(user_id)
 
     if user and session.get("username") != user.username:
         session["username"] = user.username
