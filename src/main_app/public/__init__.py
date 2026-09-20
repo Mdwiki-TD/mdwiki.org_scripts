9187a478c06328e5199c1a2722e0131b3ea7427a
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-21 01:59:44 +0300

    .

--- src/main_app/public/__init__.py
+++ src/main_app/public/__init__.py
@@ -10,7 +10,7 @@ from .auth.routes import AuthRoutes
 from .fixred import FixRedRoutes
 from .main_routes import MainRoutes
 from .newupdater.route import NewUpdaterRoutes
-from .profile import ProfileRoutes
+from .profile import ProfileView
 from .public_jobs import PublicJobsRoutes
 
 
@@ -25,7 +25,7 @@ class PublicRouteModule:
 PUBLIC_ROUTE_MODULES: list[PublicRouteModule] = [
     PublicRouteModule(MainRoutes, "main", ""),
     PublicRouteModule(AuthRoutes, "auth", ""),  # /auth
-    PublicRouteModule(ProfileRoutes, "profile", "/profile"),
+    PublicRouteModule(ProfileView, "profile", "/profile"),
     PublicRouteModule(FixRedRoutes, "fixred", "/fixred"),
     PublicRouteModule(NewUpdaterRoutes, "newupdater", "/newupdater"),
     PublicRouteModule(
